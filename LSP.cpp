6fffffe06fd64bd398b3795de9ada83d440e8724
hma-3 <maria.gula0307@gmail.com>
2023-12-16 21:59:58 +0200

    Initial commit

---
+++ LSP.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Shape {
+public:
+    virtual double area() = 0;
+};
+
+class Circle : public Shape {
+    double radius;
+public:
+    Circle(double radius)
+        : radius(radius) {}
+
+    double area() override {
+        return 3.14 * radius * radius;
+    }
+
+};
+
+class Rectangle : public Shape {
+    double width;
+    double height;
+public:
+    Rectangle(double width, double height)
+        : width(width), height(height) {}
+
+    double area() override {
+        return width * height;
+    }
+};
+
+void printArea(Shape& shape) {
+    cout << "Area: " << shape.area() << endl;
+}
+
+int main() {
+    return 0;
+}
